da69dde5f8057b0060521148a9ad85145fc22174
ygm <756949055@qq.com>
2020-01-16 23:44:08 +0800

    add arithmetic

---
+++ src/arithmetic/strings/lengthOfLongestSubstring.js
@@ -0,0 +1,36 @@
+/** 
+ * @description
+ * 给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
+ * @example "abcabcbb" -> 3
+ * @example "bbbbb" -> 1
+ * @example "pwwkew" -> 3
+ * @param {string} s
+ * @return {number}
+ * 
+ * 解题思路
+ * 1. 暴力遍历法，以每一个节点为起点遍历，知道遇到重复项，记录最长的一次。性能差。
+ * 2. 尺取法，头尾概念，设定头尾都在起点位置，开始遍历后，每遍历一个位置移动一次尾，当尾部位置的字符与头部或头部之间重复，将头移动至前重复字符后面的位置。性能优秀。
+ * 例如： gcboajsdakdjals，从g开始遍历，到第二个a发现其重复，那么实际上在第一个a之前的字符已经不存在做起点的意义，都不可能超过g为起点的长度。
+ */
+
+const lengthOfLongestSubstring = (s) => {
+  const map = {}; // 字符记录器
+  let head = 0;  // 头的位置
+  // max为结果即最大值，i实际上就是尾
+  return s.split('').reduce((max, v, i) => {
+    const mapValue = map[v];
+    // 当记录器中有值且在头或头之后重复，更新头的位置
+    if (mapValue >= head) {
+      head = mapValue + 1;
+    }
+    map[v] = i;
+    // 计算是否有增长
+    return Math.max(max, i - head + 1);
+  }, 0);
+}
+
+console.log('abcabcbb', lengthOfLongestSubstring('abcabcbb'));
+console.log('bbbbb', lengthOfLongestSubstring('bbbbb'));
+console.log('abba', lengthOfLongestSubstring('abba'));
+console.log('pwwkew', lengthOfLongestSubstring('pwwkew'));
+console.log('abcabcbb', lengthOfLongestSubstring('abcabcbb'));
---
+++ src/arithmetic/strings/longestCommonPrefix.js
@@ -0,0 +1,36 @@
+/**
+ * @description
+ * 编写一个函数来查找字符串数组中的最长公共前缀。如果不存在公共前缀，返回空字符串 ""。
+ * @example ["flower","flow","flight"] -> "fl"
+ * @example ["dog","racecar","car"] -> ""
+ * @param {string[]} strs 所有输入只包含小写字母 a-z
+ * @return {string}
+ * 
+ * 解题思路
+ * 以第一个字符串为基础，依次遍历后面的字符串，如果开头就不一样直接返回空，如果遇到不一样就截取基础值。
+ * 可以对数组先进行排序优化，如以长度，或者以开头，以开头可以倒过来遍历
+ */
+
+var longestCommonPrefix = function (strs) {
+  let res = strs[0] || '';
+  if (!res) return res;
+  const maxLen = res.length;
+  const strsLen = strs.length;
+  const startS = res[0];
+  for (let i = 1; i < strsLen; i++) {
+    const str = strs[i];
+    if (!str || str.indexOf(startS) !== 0) return '';
+    for (let j = 0; j < maxLen; j++) {
+      if (res[j] !== str[j]) {
+        res = res.slice(0, j);
+        break
+      }
+    }
+  }
+  return res;
+}
+
+console.log(["", "flow", "flight"], longestCommonPrefix(["", "flow", "flight"]));
+console.log(["dog", "racecar", "car"], longestCommonPrefix(["dog", "racecar", "car"]));
+console.log(["aa", "a"], longestCommonPrefix(["aa", "a"]));
+console.log(["c", "c"], longestCommonPrefix(["c", "c"]));
