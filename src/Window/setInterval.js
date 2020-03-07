e91625e073aa32e6653839df8e5aaafc3335875a
ygm <756949055@qq.com>
2020-03-07 20:19:41 +0800

    feat: add footer layout

--- src/Window/setInterval.js
+++ src/Window/setInterval.js
@@ -2,7 +2,7 @@
  * @description
  * 实现浏览器端setInterval API
  * 1. 记录开始时间；
- * 2. 创建递归执行函数，利用requestAnimationFrame方法，进行循环执，并保存在定时器id；
+ * 2. 创建递归执行函数，利用requestAnimationFrame方法，进行循环执行，并保存在定时器id；
  * 3. 一旦时间达到，执行回调，并且重制开始时间
  */
 
--- src/Window/setTimeout.js
+++ src/Window/setTimeout.js
@@ -2,7 +2,7 @@
  * @description
  * 实现浏览器端setTimeout API
  * 1. 创建一个内部定时器，记录开始时间；
- * 2. 创建递归执行函数，利用requestAnimationFrame方法，进行循环执，并保存在定时器id；
+ * 2. 创建递归执行函数，利用requestAnimationFrame方法，进行循环执行，并保存在定时器id；
  * 3. 一旦时间达到，执行回调，结束AnimationFrame
  */
 
